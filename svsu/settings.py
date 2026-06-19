5b9a823b2ee713c1ca83baafe2fb8abb09c1b14b
RishabhTyagii <150930058+RishabhTyagii@users.noreply.github.com>
2026-06-19 13:21:17 +0530

    wf

--- svsu/settings.py
+++ svsu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mogvm=!tz1woav@ihq09g*x9atx=ff!enw__c$832f26pme08d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
